b165b2b448e7f56ea1616cc9c59f3ef871736c19
Bokassa Jean le Grand <j_dabok@me.com>
2021-02-18 22:44:15 +0100

    completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,13 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
+import RevealOnScroll from './modules/RevealOnScroll.js';
 
 if (module.hot) {
     module.hot.accept();
 };
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 
