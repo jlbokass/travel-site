bf01686c8cd89beff1ad9f464b823af8519eeb48
Bokassa Jean le Grand <j_dabok@me.com>
2021-02-22 18:29:00 +0100

    updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,30 @@
 exports.handler = function(event, context, callback) {
+	
+	const secretContent = ` 
+	<h3>Welcome To the Secret Area</h3>
+	<p>Here we can telle you that the sky is <strong>blue</strong>, and two plus two equal four</p>
+	`
+	
+	let body 
+
+	if (event.body) {
+		body = JSON.parse(event.body)
+	} else {
+		body = {}
+	}
+
+	if (body.password == "javascript") {
+		callback(null, {
+			statusCode: 200,
+			Body: secretContent
+		})
+	} else {
+		callback(null, {
+			statusCode: 401,
+			Body: "Incorrect password"
+		})
+	}
+
 	callback(null, {
 		statusCode: 200,
 		Body: "Welcome to the secret area"
