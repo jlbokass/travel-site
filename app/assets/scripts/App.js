89ef945da4e74123f11edcaebf288bb414a4f64b
Bokassa Jean le Grand <j_dabok@me.com>
2021-02-22 09:34:49 +0100

    completed sticky-header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 
 if (module.hot) {
     module.hot.accept();
@@ -9,5 +10,6 @@ if (module.hot) {
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+let stickyHeader = new StickyHeader();
 
 
